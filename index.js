23c2163da392b4f976c852b667b55a43a1ca694d
kamilajesus <kamilajesusdev@gmail.com>
2022-12-01 22:06:35 -0300

    Adicionando bootstrap

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const app = express()
 
-app.get("/", (req, res) => {
-    res.send("Bem vindo ao meu site!")
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.get("/:nome/:lang", (req, res) => {
+    res.render("index");
 });
 
 app.listen(3000, () => {
     console.log("App rodando!")
-})
+});
