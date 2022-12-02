02f50bc51f96b6b12e09aa5dd6b202c5084af342
kamilajesus <kamilajesusdev@gmail.com>
2022-12-02 20:26:09 -0300

    Usando componentes

--- index.js
+++ index.js
@@ -4,9 +4,13 @@ const app = express()
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-app.get("/:nome/:lang", (req, res) => {
+app.get("/", (req, res) => {
     res.render("index");
 });
+
+app.get("/perguntar", (req, res) => {
+    res.render("perguntar");
+})
 
 app.listen(3000, () => {
     console.log("App rodando!")
