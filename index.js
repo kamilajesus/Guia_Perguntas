6f28ed35b53c059b2a93b3a6f7dcdb1c75d9ac32
kamilajesus <kamilajesusdev@gmail.com>
2022-12-04 18:05:17 -0300

    Colocando perguntas em ordem decrescente

--- index.js
+++ index.js
@@ -21,7 +21,9 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-    Pergunta.findAll({ raw: true }).then(perguntas => {
+    Pergunta.findAll({ raw: true, order:[
+        ['id', 'DESC']
+    ]}).then(perguntas => {
         res.render("index", {
             perguntas: perguntas
         });
