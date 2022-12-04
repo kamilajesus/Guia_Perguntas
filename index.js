18a662360a0572d583b5f0b9c46285fa1f3e58b8
kamilajesus <kamilajesusdev@gmail.com>
2022-12-04 18:01:41 -0300

    Estilizando a rota de perguntas

--- index.js
+++ index.js
@@ -21,7 +21,11 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-    res.render("index");
+    Pergunta.findAll({ raw: true }).then(perguntas => {
+        res.render("index", {
+            perguntas: perguntas
+        });
+    }); 
 });
 
 app.get("/perguntar", (req, res) => {
