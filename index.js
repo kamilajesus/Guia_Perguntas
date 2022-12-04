e630b460bb7d5245135ff3f508ab2b41b8fa02b2
kamilajesus <kamilajesusdev@gmail.com>
2022-12-04 17:28:43 -0300

    Salvando perguntas no BD

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express()
 const bodyParser = require("body-parser");
 const connection = require("./database/database");
-const perguntaModel = require("./database/Pergunta");
+const Pergunta = require("./database/Pergunta");
 
 connection
     .authenticate()
@@ -31,7 +31,12 @@ app.get("/perguntar", (req, res) => {
 app.post("/salvarpergunta", (req, res) => {
     var titulo = req.body.titulo;
     var descricao = req.body.descricao;
-    res.send("formulario recebido! titulo " + titulo + " " + " descricao " + descricao)
+    Pergunta.create({
+        titulo: titulo,
+        descricao: descricao
+    }).then(() => {
+        res.redirect("/")
+    });
 });
 
 app.listen(3000, () => {
