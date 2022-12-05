e29cf2ae28d2dbfc3bc4b4df5f683ab6ee2407b9
kamilajesus <kamilajesusdev@gmail.com>
2022-12-05 19:20:58 -0300

    frontend página de pergunta

--- index.js
+++ index.js
@@ -51,7 +51,9 @@ app.get("/pergunta/:id", (req, res) => {
         where: {id:id}
     }).then(pergunta => {
         if(pergunta != undefined){
-            res.render("pergunta");
+            res.render("pergunta", {
+                pergunta: pergunta
+            });
         }else {
             res.redirect("/");
         }
