0d1dc5adcca5a9c9edd161b969375ea24305f8e5
kamilajesus <kamilajesusdev@gmail.com>
2022-12-05 19:57:06 -0300

    Respondendo perguntas

--- index.js
+++ index.js
@@ -61,6 +61,17 @@ app.get("/pergunta/:id", (req, res) => {
     });
 })
 
+app.post("/responder", (req, res) => {
+    var corpo = req.body.corpo;
+    var perguntaId = req.body.pergunta;
+    Resposta.create({
+        corpo: corpo,
+        perguntaId: perguntaId
+    }).then(() => {
+        res.redirect("/pergunta/"+perguntaId);
+    })
+});
+
 app.listen(3000, () => {
     console.log("App rodando!")
 });
